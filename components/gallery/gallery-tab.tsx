effc871d4a2d0974ce9eda0c5632e3a7c38cdb3b
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-31 14:40:35 +0100

    Created product page

---
+++ components/gallery/gallery-tab.tsx
@@ -0,0 +1,31 @@
+import NextImage from "next/image";
+import { Tab } from "@headlessui/react";
+
+import { cn } from "@/lib/utils";
+import { Image } from "@/types";
+
+interface GalleryTabProps {
+  image: Image;
+}
+
+const GalleryTab: React.FC<GalleryTabProps> = ({ image }) => {
+  return (
+    <Tab className="relative flex items-center justify-center bg-white rounded-md cursor-pointer aspect-square">
+      {({ selected }) => (
+        <div>
+          <span className="absolute inset-0 w-full h-full overflow-hidden rounded-md aspect-square">
+            <NextImage fill src={image.url} alt="" className="object-cover object-center" />
+          </span>
+          <span
+            className={cn(
+              "absolute inset-0 rounded-md ring-2 ring-offset-2",
+              selected ? "ring-black" : "ring-transparent"
+            )}
+          />
+        </div>
+      )}
+    </Tab>
+  );
+};
+
+export default GalleryTab;
---
+++ components/gallery/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import NextImage from "next/image";
+import { Tab } from "@headlessui/react";
+
+import { Image } from "@/types";
+
+import GalleryTab from "./gallery-tab";
+
+interface GalleryProps {
+  images: Image[];
+}
+
+const Gallery: React.FC<GalleryProps> = ({ images = [] }) => {
+  return (
+    <Tab.Group as="div" className="flex flex-col-reverse">
+      <div className="hidden w-full max-w-2xl mx-auto mt-6 sm:block lg:max-w-none">
+        <Tab.List className="grid grid-cols-4 gap-6">
+          {images.map((image) => (
+            <GalleryTab key={image.id} image={image} />
+          ))}
+        </Tab.List>
+      </div>
+      <Tab.Panels className="w-full aspect-square">
+        {images.map((image) => (
+          <Tab.Panel key={image.id}>
+            <div className="relative w-full h-full overflow-hidden aspect-square sm:rounded-lg">
+              <NextImage fill src={image.url} alt="Image" className="object-cover object-center" />
+            </div>
+          </Tab.Panel>
+        ))}
+      </Tab.Panels>
+    </Tab.Group>
+  );
+};
+
+export default Gallery;
